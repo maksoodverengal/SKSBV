36e07097995c9b6e41f9b10dee28d163c2ccee89
maksoodverengal <maksoodverengal@gmail.com>
2026-09-12 14:36:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-// Paste your deployed Google Apps Script Web App URL between the quotes.
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwGnTSwgNN-bR-9MOFodEhaB9OMHNUFacevExeRoST8I4F0bbnH32EjHgZEySPmJzie/exec";
+const GOOGLE_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbwGnTSwgNN-bR-9MOFodEhaB9OMHNUFacevExeRoST8I4F0bbnH32EjHgZEySPmJzie/exec";
